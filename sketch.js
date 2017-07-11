3a0b3d69f46ed6991fcf24ca53dc48626797147d
Hugo <HugovdV@outlook.com>
2017-07-11 22:11:51 +0200

    The bird is flapping.

--- sketch.js
+++ sketch.js
@@ -1,32 +1,44 @@
-var g = 5;
 var bird;
 
 function setup() {
   createCanvas(500,500);
-  bird = new Bird(50, 250);
+  bird = new Bird(75, height / 2);
 }
 
 function draw() {
   background(50);
+  bird.update();
   bird.show();
 }
 
 function keyPressed() {
   if(keyCode == 32) {
-    bird.move(10);
+    bird.jump();
   }
 }
 
 function Bird(x, y) {
   this.x = x;
   this.y = y;
+  this.gravity = 0.75;
   this.speed = 0;
-  this.acc = 0;
 
-  this.move = function(a) {
-    this.acc += a;
-    this.speed += this.acc;
-    this.y -= this.speed;
+  this.update = function() {
+    this.speed += this.gravity;
+    this.speed *= 0.9;
+    this.y += this.speed;
+
+    if (this.y < 0) {
+      this.y = 0;
+    }
+    if (this.y > height) {
+      this.y = height;
+    }
+  }
+
+  this.jump = function() {
+    this.speed -= 12.5;
+    this.y += this.speed;
   }
 
   this.show = function() {
